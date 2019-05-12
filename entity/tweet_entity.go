f64872fde4c1fc937a5abda8f01c99ed61145e73
Koichi Taura <taura.koichi@gmail.com>
2019-05-12 17:13:43 +0900

    add entity of tweet

---
+++ entity/tweet_entity.go
@@ -0,0 +1,26 @@
+package entity
+
+/*
+Tweet structure to insert mongo db
+*/
+type Tweet struct {
+	ID    int64
+	IDStr string
+	Text  string
+}
+
+/*
+User structure of twitter
+*/
+type User struct {
+	ID                   int64
+	IDStr                string
+	Name                 string
+	ScreenName           string
+	Location             string
+	URL                  string
+	Description          string
+	FollowersCount       int
+	FriendsCount         int
+	ProfileImageURLHTTPS string
+}
