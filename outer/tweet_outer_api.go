98ef15c41d1b2517d677787552bcb6cd099fb824
Taurin190 <taura.koichi@gmail.com>
2019-05-14 08:54:32 +0900

    implement outer api structure

--- outer/tweet_outer_api.go
+++ outer/tweet_outer_api.go
@@ -1,8 +1,12 @@
 package outer
 
 import (
+	"log"
+	"net/url"
+
 	"../config"
 	"../entity"
+	"github.com/ChimeraCoder/anaconda"
 )
 
 type TweetOuterAPI interface {
@@ -20,6 +24,20 @@ func GetTweetOuterAPI(c *config.Config) TweetOuterAPI {
 }
 
 func (outerAPI *tweetOuterAPI) GetTweets() ([]entity.Tweet, error) {
-	var tweets []entity.Tweet
-	return tweets, nil
+	tweets, err := outerAPI.getTweetData()
+	return tweets, err
+}
+
+func (outerAPI *tweetOuterAPI) getTweetData() ([]entity.Tweet, error) {
+	anaconda.SetConsumerKey(outerAPI.Config.Twitter.ConsumerKey)
+	anaconda.SetConsumerSecret(outerAPI.Config.Twitter.ConsumerSecret)
+	api := anaconda.NewTwitterApi(outerAPI.Config.Twitter.AccessToken, outerAPI.Config.Twitter.AccessSecret)
+	v := url.Values{}
+	v.Set("count", "30")
+	searchResult, err := api.GetSearch("golang", v)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	return entity.ConvertTweetsStruct(searchResult.Statuses), nil
 }
