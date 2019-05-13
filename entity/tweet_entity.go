844a472ac14377d245362207d311ffe741f07103
Koichi Taura <taura.koichi@gmail.com>
2019-05-13 22:38:16 +0900

    add convert function of entity

--- entity/tweet_entity.go
+++ entity/tweet_entity.go
@@ -1,5 +1,9 @@
 package entity
 
+import (
+	"github.com/ChimeraCoder/anaconda"
+)
+
 /*
 Tweet structure to insert mongo db
 */
@@ -7,6 +11,7 @@ type Tweet struct {
 	ID    int64
 	IDStr string
 	Text  string
+	User
 }
 
 /*
@@ -23,4 +28,34 @@ type User struct {
 	FollowersCount       int
 	FriendsCount         int
 	ProfileImageURLHTTPS string
+}
+
+/*
+ConvertTweetsStruct : From anaconda.Tweet, convert tweet structure
+*/
+func ConvertTweetsStruct(anacondaTweets []anaconda.Tweet) []Tweet {
+	var tweets []Tweet
+	for _, anacondaTweet := range anacondaTweets {
+		var tweet Tweet
+		tweet.ID = anacondaTweet.Id
+		tweet.IDStr = anacondaTweet.IdStr
+		tweet.Text = anacondaTweet.Text
+
+		var user User
+		user.ID = anacondaTweet.User.Id
+		user.IDStr = anacondaTweet.User.IdStr
+		user.Name = anacondaTweet.User.Name
+		user.ScreenName = anacondaTweet.User.ScreenName
+		user.Location = anacondaTweet.User.Location
+		user.URL = anacondaTweet.User.URL
+		user.Description = anacondaTweet.User.Description
+		user.FollowersCount = anacondaTweet.User.FollowersCount
+		user.FriendsCount = anacondaTweet.User.FriendsCount
+		user.ProfileImageURLHTTPS = anacondaTweet.User.ProfileBackgroundImageUrlHttps
+
+		tweet.User = user
+
+		tweets = append(tweets, tweet)
+	}
+	return tweets
 }
